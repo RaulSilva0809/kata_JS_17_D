dd26167033478eaf68dea2ad50925f044defec26
RaulSilva0809 <tecnosis08@gmail.com>
2022-05-31 21:02:29 -0500

    Primera clase practica

---
+++ 0_Intro_JS/main.js
@@ -0,0 +1,17 @@
+//Como poner una laerta con JS
+alert('Hola Mundo')
+
+//Enviar mensaje a la consola
+console.log('Mensaje log')
+
+//Enviar un arror
+console.error('ERROR')
+
+//Enviar advertencia
+console.warn('Cuidado cuidado, cuidado con la viborita')
+
+//Entrada
+prompt('Introduce tu nombre')
+prompt('Introduce tu apellido')
+
+document.write('Texto')
---
+++ 1_Variables_y_datos/app.js
@@ -0,0 +1,46 @@
+//Creacio y asignacion a una variable
+var nombre;
+nombre = 'Raul';
+var nombreUser = 'Fabrizio';
+
+//Strings
+var pais='Colombia';
+var fraseSecreta='Primer amigo de la infancia';
+var nombrePrincipal='';
+var letra='a';
+
+//Las variables se pueden reasingar
+pais='Mexico';
+letra = 'x';
+
+//Podemos acceder a la info almacenada en las variables
+console.log('hola');
+console.log(fraseSecreta);
+
+//number
+var edad;
+edad=20;
+var telefono=154648;
+var Costo=20;
+var resultado=-50;
+var promedio=9.87
+
+//las variables se pueden reasignar
+costo=10;
+resultado=0;
+
+var numCelular = '+52487965';
+var numOficina = '145553';
+
+//concatenar do o mas String
+var info = nombre + pais;
+console.log(info);
+console.log(pais + Costo)
+
+//Template String*
+
+//BOOLEAN
+var esAdulto=true;
+var graduado=false;
+var cumple=true;
+var Mexicano=false;
