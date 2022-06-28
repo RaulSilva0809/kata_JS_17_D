922c1bd7107176a814abbf25d569106bcca145b8
RaulSilva0809 <tecnosis08@gmail.com>
2022-06-27 20:18:14 -0500

    Temas de entrenamiento

---
+++ Challenge_JS/app.js
@@ -0,0 +1,56 @@
+let info = [
+    {
+        nombre: "Diego",
+        materias: [
+            {
+                nombreMateria: "español",
+                calificacion: 100
+            },
+            {
+                nombreMateria: "matemáticas",
+                calificacion: 70
+            },
+            {
+                nombreMateria: "ciencias",
+                calificacion: 55
+            }
+        ]
+    },
+    {
+        nombre: "Jorge",
+        materias: [
+            {
+                nombreMateria: "español",
+                calificacion: 60
+            },
+            {
+                nombreMateria: "matemáticas",
+                calificacion: 65
+            },
+            {
+                nombreMateria: "ciencias",
+                calificacion: 80
+            }
+        ]
+    }
+]
+
+// 1.- Ingresar nombre
+// 2.- Verán un menú de opciones
+    // 1. Ver todas su calificaciones.
+    // 2. Conocer su promedio.
+    // 3. Conocer su calificación más alta.
+    // 4. Conocer su calificación más baja.
+    function nombres(){
+    let name = prompt('Ingresa el nombre del alumno');
+    }
+
+    function calificaciones(nombre){
+        for(let i = 0; i < info.length; i++){
+            console.log(cuentas[i].nombre)
+            return
+        }
+    }
+    console.log(calificaciones('Diego'));
+
+   // checar video 14 fecha 
